1087c6f4382f4c24e28a562bf7ef8d5a7d1b15f4
Stefan Wurm <stefan.wurm@sibvisions.com>
2012-05-14 14:28:14 +0000

    git-svn-id: https://svn.code.sf.net/p/jvxee/code/trunk/java@4 dcbe08a9-ddcd-4ebb-a302-f94f26c48e26

---
+++ example/JVx EE Example/rad/apps/jvxee/src.server/apps/jvxee/eao/CustomerEAO.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2009 SIB Visions GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ * History
+ *
+ * 09.05.2012 - [SW] - creation
+ */
+package apps.jvxee.eao;
+
+import javax.persistence.EntityManager;
+
+import apps.jvxee.entity.Customer;
+
+import com.sibvisions.rad.persist.jpa.EAOMethod;
+import com.sibvisions.rad.persist.jpa.EAOMethod.EAO;
+
+public class CustomerEAO {
+
+	private EntityManager entityManager;
+	
+	public void setEntityManager(EntityManager entityManager) {
+		this.entityManager = entityManager;
+	}
+	
+	@EAOMethod(methodIdentifier = EAO.DELETE)
+	public void deleteCustomer(Customer customer) {
+
+        entityManager.getTransaction().begin();
+    	
+        if(customer.getEducations().size() > 0) {
+        	customer.getEducations().clear();
+        }
+        
+        if(customer.getAddresses().size() > 0) {
+        	customer.getEducations().clear();
+        }
+        
+        entityManager.remove(customer);
+                
+        entityManager.getTransaction().commit();
+
+	}
+	
+}
---
+++ example/JVx EE Example/rad/apps/jvxee/src.server/apps/jvxee/entity/CustomerContact.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright 2009 SIB Visions GmbH
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not
+ * use this file except in compliance with the License. You may obtain a copy of
+ * the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+ * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+ * License for the specific language governing permissions and limitations under
+ * the License.
+ *
+ *
+ * History
+ *
+ * 09.05.2012 - [SW] - creation
+ */
+package apps.jvxee.entity;
+
+import javax.persistence.Embeddable;
+
+@Embeddable
+public class CustomerContact {
+
+    private String telephone_private;
+    private String telephone_office;
+    private String email;
+    
+	public String getTelephone_private() {
+		return telephone_private;
+	}
+	public void setTelephone_private(String telephone_private) {
+		this.telephone_private = telephone_private;
+	}
+	public String getTelephone_office() {
+		return telephone_office;
+	}
+	public void setTelephone_office(String telephone_office) {
+		this.telephone_office = telephone_office;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}   
+    
+}
